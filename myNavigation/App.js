ccc8cbf306e934bd714859e39b6831c3e17e2d65
Rodrigo Pereira Couto <rodrigopereiracouto@hotmail.com>
2023-11-22 15:13:59 -0300

    Stack Navigation

---
+++ myNavigation/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Contacts from "./src/pages/Contacts";
+import Information from "./src/pages/Information";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Contacts" component={Contacts} />
+        <Stack.Screen name="Information" component={Information} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
