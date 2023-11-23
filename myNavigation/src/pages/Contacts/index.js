2cb12badf31baf4ffa2c0ed6374ef4f41f6be54d
Rodrigo Pereira Couto <rodrigopereiracouto@hotmail.com>
2023-11-23 14:03:49 -0300

    Nesting multiple navigators

--- myNavigation/src/pages/Contacts/index.js
+++ myNavigation/src/pages/Contacts/index.js
@@ -3,11 +3,47 @@ import { View, Text } from "react-native";
 
 export default function Contacts({ navigation }) {
   return (
-    <View>
-      <Text>Joao</Text>
-      <Text onPress={() => navigation.navigate("Information")}>
-        Information...
-      </Text>
+    <View style={{ marginTop: 60 }}>
+      <View>
+        <Text>Nome: Joao Silva</Text>
+        <Text>Telefone: (11) 99999-9999</Text>
+        <Text>Idade: 30</Text>
+        <Text>Profissao: Desenvolvedor</Text>
+        <Text
+          onPress={() =>
+            navigation.navigate("Information", {
+              nome: "Joao Silva",
+              telefone: "(11) 99999-9999",
+              endereco: "Rua dos tals",
+              numero: "123",
+              profissao: "Desenvolvedor",
+              email: "joao@email.com.br",
+            })
+          }
+        >
+          Information...
+        </Text>
+      </View>
+      <View style={{ marginTop: 60 }}>
+        <Text>Nome: Amanda Silva</Text>
+        <Text>Telefone: (11) 8888-8888</Text>
+        <Text>Idade: 22</Text>
+        <Text>Profissao: Desenvolvedor</Text>
+        <Text
+          onPress={() =>
+            navigation.navigate("Information", {
+              nome: "Amanda Silva",
+              telefone: "(11) 88888-8888",
+              endereco: "Rua dos tals 2",
+              numero: "366",
+              profissao: "Desenvolvedor Senior",
+              email: "amanda@email.com.br",
+            })
+          }
+        >
+          Information...
+        </Text>
+      </View>
     </View>
   );
 }
--- myNavigation/src/pages/Information/index.js
+++ myNavigation/src/pages/Information/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Information() {
+export default function Information({ route }) {
   return (
     <View>
-      <Text>Information</Text>
+      <Text>Nome: {route.params?.nome}</Text>
+      <Text>Telefone:{route.params?.telefone} </Text>
+      <Text>Endereço:{route.params?.endereco}</Text>
+      <Text>Numero: {route.params?.numero}</Text>
+      <Text>Profissao: {route.params?.profissao}</Text>
+      <Text>Email: {route.params?.email}</Text>
     </View>
   );
 }
